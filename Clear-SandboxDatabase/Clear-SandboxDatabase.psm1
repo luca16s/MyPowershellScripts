30376dddacf6c35280e0cfb3ea0af4825c39910f
luca16s <luca.gian16@outlook.com>
2018-11-18 23:36:54 -0200

    Added Module Clear-SandboxDatabase

---
+++ Clear-SandboxDatabase/Clear-SandboxDatabase.psm1
@@ -0,0 +1,27 @@
+function Clear-SandboxDatabase {<#
+    .SYNOPSIS
+      Script to clean database.
+    .DESCRIPTION
+      Script that execute Flyway Clean and clear the database.
+    .PARAMETER VagrantUpSuceed
+        Parameter to indicate if Virtualbox was restored with no erreros.
+    .INPUTS
+      <Inputs if any, otherwise state None>
+    .OUTPUTS
+      <Outputs if any, otherwise state None - example: Log file stored in C:\Windows\Temp\<name>.log>
+    .NOTES
+      Version:        1.0
+      Author:         Gian Luca da Silva Figueiredo
+      Creation Date:  18/11/2018
+      Purpose/Change: Initial script development
+    .EXAMPLE
+      <Example goes here. Repeat this attribute for more than one example>
+   #>
+	param (
+		$VagrantUpSuceed = $true
+	)
+	if ($VagrantUpSuceed -eq $true) {
+		flyway.cmd clean
+    }
+}
+Export-ModuleMember -Function Clear-SandboxDatabase
