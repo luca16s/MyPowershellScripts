ee7d9e73f8260ac714250a7d6fa1a916366e11ff
luca16s <luca.gian16@outlook.com>
2018-11-18 22:13:51 -0200

    Added Module Write-LineSeparator

---
+++ Write-LineSeparator/Write-LineSeparator.psm1
@@ -0,0 +1,25 @@
+Function Write-LineSeparator {
+    <#
+     .SYNOPSIS
+       Script para inserir separador de linha.
+     .DESCRIPTION
+       Script para inserir uns separadores de linhas pós scripts executados.
+     .PARAMETER <Parameter_Name>
+         <Brief description of parameter input required. Repeat this attribute if required>
+     .INPUTS
+       <Inputs if any, otherwise state None>
+     .OUTPUTS
+       <Outputs if any, otherwise state None - example: Log file stored in C:\Windows\Temp\<name>.log>
+     .NOTES
+       Version:        1.0
+       Author:         Gian Luca da Silva Figueiredo
+       Creation Date:  18/11/2018
+       Purpose/Change: Initial script development
+     
+     .EXAMPLE
+       <Example goes here. Repeat this attribute for more than one example>
+    #>
+	Write-Output("---------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+	Write-Output("#####################################################################################################################################################################")
+	Write-Output("---------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+}
